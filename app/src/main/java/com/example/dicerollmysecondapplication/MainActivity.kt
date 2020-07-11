7d05bc6c36e2d648f1c478c55e432fbfef4df3e1
MSRAJAWAT298 <msrajawat298@gmail.com>
2020-07-11 16:51:54 +0530

    work in progress

--- app/src/main/java/com/example/dicerollmysecondapplication/MainActivity.kt
+++ app/src/main/java/com/example/dicerollmysecondapplication/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.dicerollmysecondapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton : Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener{
+            val number = rollDice()
+            Log.i("MainActivity","Button  is clicked ! $number")
+
+        }
+    }
+
+    private fun rollDice() {
+        val randomInt = Random.nextInt(6)+1
+        //Log.i("MainActivity","Button  is clicked ! $randomInt" )
+
     }
 }
